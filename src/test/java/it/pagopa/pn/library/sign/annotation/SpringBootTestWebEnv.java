2e6e276e32e7f01bd38c3bed9df363578084db67
michelescara <michele.scaramuzzino@gmail.com>
2024-03-19 16:56:16 +0100

    PN-10293: Removed unused imports.

--- src/test/java/it/pagopa/pn/library/sign/annotation/SpringBootTestWebEnv.java
+++ src/test/java/it/pagopa/pn/library/sign/annotation/SpringBootTestWebEnv.java
@@ -1,9 +1,7 @@
 package it.pagopa.pn.library.sign.annotation;
 
 import it.pagopa.pn.library.sign.TestConfig;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,6 +10,5 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {TestConfig.class})
 public @interface SpringBootTestWebEnv {}
